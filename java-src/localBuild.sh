3798f4a8c3e070578b0545139a12e466a035d6e1
Dominic Watson <watson.dominic@gmail.com>
2026-01-12 11:50:22 +0000

    Ditch no longer working maven ant-run plugin approach to moving a file

--- java-src/localBuild.sh
+++ java-src/localBuild.sh
@@ -2,6 +2,7 @@
 
 rm -rf artifacts/*
 mvn package || exit 1
+cp target/cbehcache-1.0.0-jar-with-dependencies.jar artifacts/cbehcache-1.0.0.jar
 cd artifacts
 unzip cbehcache-1.0.0.jar
 echo "Manifest-Version: 1.0
